c5daddfe47de2e9a1316c7fa35c6aad9235f36dd
suphakiat kiatkanya <suphakiat08@gmail.com>
2017-10-21 13:50:16 +0700

    create BookLookup

---
+++ BookLookup.js
@@ -0,0 +1,10 @@
+function BookLookup(AmazonService) {
+    this.AmazonService = AmazonService
+
+    this.search = function(isbn) {
+        let obj = this.AmazonService(isbn)
+        return {"bookname": obj.title, "cover": obj.image, "isbn": obj.isbn}
+    }
+}
+
+module.exports = BookLookup
---
+++ BookLookup.spec.js
@@ -0,0 +1,26 @@
+const BookShelfApp = require('./BookLookup.js')
+
+describe('BookLookup App', function() {
+    it('Search function with 001925', function() {
+        const returnObj = {
+            title: 'reborn',
+            image: '001925.png',
+            isbn: '001925'
+        }
+        
+        let isbn = '001925'
+        
+        const mockFn = jest.fn(isbn)
+            .mockReturnValue(returnObj)
+
+        let app = new BookShelfApp(mockFn)
+        let result = app.search(isbn)
+
+        expect(result.bookname).toBe('reborn')
+        expect(result.cover).toBe('001925.png')
+        expect(result.isbn).toBe('001925')
+        
+        expect(mockFn).toHaveBeenCalled()
+        expect(mockFn).toHaveBeenCalledWith(isbn)
+    })
+})
